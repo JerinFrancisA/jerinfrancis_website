aa81fb680b012773d3ab00acdfa2f7a9f122460c
JerinFrancisA <jerinfrancis77@gmail.com>
2019-09-14 13:57:43 +0530

    font chng

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Jerin Francis',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'GoogleSansRegular',
+        textTheme: TextTheme(
+          headline: TextStyle(
+            fontFamily: 'GoogleSansRegular',
+            letterSpacing: 4.0,
+            fontSize: 50.0,
+          ),
+          title: TextStyle(
+            fontFamily: 'GoogleSansRegular',
+            letterSpacing: 3.0,
+            fontSize: 30.0,
+          ),
+          body1: TextStyle(
+            fontFamily: 'GoogleSansRegular',
+            letterSpacing: 2.0,
+            fontSize: 20.0,
+          ),
+          body2: TextStyle(
+            fontFamily: 'GoogleSansRegular',
+            letterSpacing: 2.0,
+            fontSize: 10.0,
+          ),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -16,59 +39,86 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('JERIN FRANCIS'),
-                  Text('PROJECTS'),
-                  Text('RESUME'),
-                  Text('CONTACT'),
-                ],
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      'JERIN FRANCIS',
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                    Text(
+                      'PROJECTS',
+                      style: Theme.of(context).textTheme.body1,
+                    ),
+                    Text(
+                      'RESUME',
+                      style: Theme.of(context).textTheme.body1,
+                    ),
+                    Text(
+                      'CONTACT',
+                      style: Theme.of(context).textTheme.body1,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              flex: 7,
-              child: Row(
-                children: <Widget>[
-                  Image.asset('images/me.jpg'),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text('Hey! I\'m Jerin Francis'),
-                      Text('App Developer'),
-                      Text('Student of Ramaiah Institute of Technology'),
-                    ],
-                  ),
-                ],
+              Expanded(
+                flex: 6,
+                child: Row(
+                  children: <Widget>[
+                    Image.asset('assets/me.jpg'),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          'Hey! I\'m Jerin Francis',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        Text(
+                          'App Developer',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        Text(
+                          'Flutter and Dart enthusiast',
+                          style: Theme.of(context).textTheme.title
+                        ),
+                        Text(
+                          'Student of Ramaiah Institute of Technology',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('a'),
-                  Text('a'),
-                  Text('a'),
-                  Text('a'),
-                  Text('a'),
-                  Text('a'),
-                ],
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('a'),
+                    Text('a'),
+                    Text('a'),
+                    Text('a'),
+                    Text('a'),
+                    Text('a'),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
