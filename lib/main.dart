31455af6fd9811599563da5c28a0136b86f24f53
JerinFrancisA <jerinfrancis77@gmail.com>
2019-09-14 14:11:51 +0530

    font chngss

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,19 @@ class MyApp extends StatelessWidget {
         fontFamily: 'GoogleSansRegular',
         textTheme: TextTheme(
           headline: TextStyle(
-            fontFamily: 'GoogleSansRegular',
             letterSpacing: 4.0,
             fontSize: 50.0,
+            fontWeight: FontWeight.bold,
           ),
           title: TextStyle(
-            fontFamily: 'GoogleSansRegular',
             letterSpacing: 3.0,
-            fontSize: 30.0,
+            fontSize: 25.0,
           ),
           body1: TextStyle(
-            fontFamily: 'GoogleSansRegular',
             letterSpacing: 2.0,
             fontSize: 20.0,
           ),
           body2: TextStyle(
-            fontFamily: 'GoogleSansRegular',
             letterSpacing: 2.0,
             fontSize: 10.0,
           ),
