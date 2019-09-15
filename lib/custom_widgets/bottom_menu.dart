e08f6197caef1be7fb86b219ae91fa69fd7a7176
JerinFrancisA <jerinfrancis77@gmail.com>
2019-09-15 14:35:34 +0530

    reformat 2

---
+++ lib/custom_widgets/bottom_menu.dart
@@ -0,0 +1,64 @@
+import 'dart:html' as html;
+
+import 'package:flutter_web/material.dart';
+
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            GestureDetector(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1157035760085684224/iuxTnT5g_400x400.jpg',
+                ),
+              ),
+              onTap: () {
+                html.window.open("https://github.com/JerinFrancisA", "GitHub");
+              },
+            ),
+            GestureDetector(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1068098089125171200/jwQJ5rLn.jpg',
+                ),
+              ),
+              onTap: () {
+                html.window
+                    .open("https://twitter.com/jerinfrancis4", "Twitter");
+              },
+            ),
+            GestureDetector(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1111729635610382336/_65QFl7B.png',
+                ),
+              ),
+              onTap: () {
+                html.window.open(
+                    "https://www.linkedin.com/in/jerinfrancis77/", "LinkedIn");
+              },
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Jerin Francis ©️ 2019',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.body2.copyWith(
+                color: Colors.grey,
+              ),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:html' as html;
-
 import 'package:flutter_web/material.dart';
 import 'package:jerinfrancis_website/utilities/constants.dart';
 
+import 'custom_widgets/bottom_menu.dart';
 import 'custom_widgets/top_menu.dart';
 import 'custom_widgets/body.dart';
 
@@ -39,59 +38,7 @@ class MyHomePage extends StatelessWidget {
               ),
               Expanded(
                 flex: 1,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        GestureDetector(
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                              'https://pbs.twimg.com/profile_images/1157035760085684224/iuxTnT5g_400x400.jpg',
-                            ),
-                          ),
-                          onTap: () {
-                            html.window.open(
-                                "https://github.com/JerinFrancisA", "GitHub");
-                          },
-                        ),
-                        GestureDetector(
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                              'https://pbs.twimg.com/profile_images/1068098089125171200/jwQJ5rLn.jpg',
-                            ),
-                          ),
-                          onTap: () {
-                            html.window.open(
-                                "https://twitter.com/jerinfrancis4", "Twitter");
-                          },
-                        ),
-                        GestureDetector(
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                              'https://pbs.twimg.com/profile_images/1111729635610382336/_65QFl7B.png',
-                            ),
-                          ),
-                          onTap: () {
-                            html.window.open(
-                                "https://www.linkedin.com/in/jerinfrancis77/",
-                                "LinkedIn");
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Jerin Francis ©️ 2019',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.body2.copyWith(
-                            color: Colors.grey,
-                          ),
-                    )
-                  ],
-                ),
+                child: BottomMenu(),
               ),
             ],
           ),
